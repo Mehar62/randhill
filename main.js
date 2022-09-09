65718364baa5593d0947082df2087992eb9cdecf
Mehar62 <78727941+Mehar62@users.noreply.github.com>
2022-09-09 19:29:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function callNavbar(e) {
+    e.classList.toggle("changed-icon");
+    document.getElementById("navbar").classList.toggle("nav");
+}
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
